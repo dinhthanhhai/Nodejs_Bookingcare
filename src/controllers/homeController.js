e342b988ec4e7a325b3df93403285c095f71efad
dinhthanhhai <dthk164@gmail.com>
2023-07-13 23:50:38 +0700

    connect db

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,6 +1,15 @@
+import db from "../models/index"
 
-let getHomePage = (req, res) => {
-    return res.render('homepage.ejs');
+let getHomePage = async (req, res) => {
+    try {
+        let data = await db.User.findAll();
+        console.log(data)
+        return res.render('homepage.ejs', {
+            data: JSON.stringify(data)
+        });
+    } catch (e) {
+        console.log(e);
+    }
 
 }
 
