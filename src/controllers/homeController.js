f8324ef3716fc617b836d213228eaa6c083c7955
dinhthanhhai <dthk164@gmail.com>
2023-09-15 22:42:05 +0700

    crud-create user

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,4 +1,5 @@
 import db from "../models/index"
+import CRUDService from "../services/CRUDService"
 
 let getHomePage = async (req, res) => {
     try {
@@ -10,9 +11,20 @@ let getHomePage = async (req, res) => {
     } catch (e) {
         console.log(e);
     }
+}
 
+let getCRUD = (req, res) => {
+    return res.render('crud.ejs');
+}
+
+let postCRUD = async (req, res) => {
+    let message = await CRUDService.createNewUser(req.body);
+    console.log(message);
+    return res.send('Post crud from server');
 }
 
 module.exports = {
     getHomePage: getHomePage,
+    getCRUD: getCRUD,
+    postCRUD: postCRUD,
 }
--- src/route/web.js
+++ src/route/web.js
@@ -5,11 +5,9 @@ let router = express.Router();
 
 let initWebRoutes = (app) => {
     router.get('/', homeController.getHomePage);
-
-
-    router.get('/dinhthanhhai', (req, res) => {
-        return res.send('Hello dinhthanhhai');
-    });
+    router.get('/about', homeController.getHomePage);
+    router.get('/crud', homeController.getCRUD);
+    router.post('/post-crud', homeController.postCRUD);
 
     return app.use("/", router);
 }
